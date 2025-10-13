dc442ed4d04a4c9a72f1c3387fd76d3f985da6c1
Matteo Di Rollo <matteo.dirollo@icloud.com>
2025-10-13 22:21:05 +0200

    GA integration

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -25,12 +25,12 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html className={cn(GeistSans.variable, GeistMono.variable)} lang="en" suppressHydrationWarning>
       <head>
         <InitTheme />
-        <GoogleAnalytics />
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
         <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
       </head>
       <body>
         <Providers>
+          <GoogleAnalytics />
           <AdminBar
             adminBarProps={{
               preview: isEnabled,
