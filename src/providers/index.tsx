0fc7b877fba178d8047ff93bc0b9317ebbad4de8
Matteo Di Rollo <matteo.dirollo@icloud.com>
2025-10-13 20:29:13 +0200

    speed insights

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 
 import { HeaderThemeProvider } from './HeaderTheme'
 import { ThemeProvider } from './Theme'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export const Providers: React.FC<{
   children: React.ReactNode
 }> = ({ children }) => {
   return (
     <ThemeProvider>
-      <HeaderThemeProvider>{children}</HeaderThemeProvider>
+      <HeaderThemeProvider>
+        <SpeedInsights />
+        {children}
+      </HeaderThemeProvider>
     </ThemeProvider>
   )
 }
